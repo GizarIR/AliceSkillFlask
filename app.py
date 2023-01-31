b9bf489713b50ce8b2a44ba6c6ae8e6e08745371
Gizar <gizarir@gmail.com>
2023-02-01 00:00:50 +0200

    First commit. Setuped simple Flask app

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+import logging
+import json
+
+app = Flask(__name__)
+
+# logging.basicConfig(level=logging.DEBUG)
+#
+# @app.route("/", methods=["POST"])
+# def main(request):
+#     logging.info(request.json)
+#
+#     response = {
+#         "version": request.json["version"],
+#         "session": request.json["session"],
+#         "response": {
+#             "end_session": False
+#         }
+#     }
+#
+#     req = request.json
+#     if req["session"]["new"]:
+#         response["response"]["text"] = "Привет! Как твои дела? Как отметитил новый год?"
+#     else:
+#         if req["request"]["original_utterance"].lower() in ["хорошо","отлично"]:
+#             response["response"]["text"] = "Супер! Я за вас рада!"
+#         elif req["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
+#             response["response"]["text"] = "Это печально... нужно было позвать меня!"
+#
+#     return json.dumps(response)
+
+
+@app.route('/')
+def index():
+    return 'Hello World!!!'
+
+if __name__ == "__main__":
+    app.run()
