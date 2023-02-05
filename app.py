ffb5f125cfa8f714381c16d75f4563441f313275
JuliaMISH <juliamish@mail.ru>
2023-02-05 16:35:29 +0300

    test_commi

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def main():
         if req["request"]["original_utterance"].lower() in ["хорошо","отлично"]:
             response["response"]["text"] = "Супер! Я за вас рада!"
         elif req["request"]["original_utterance"].lower() in ["плохо", "скучно"]:
-            response["response"]["text"] = "Это печально... нужно было позвать меня!"
+            response["response"]["text"] = "Привет, с новым годом!"
 
     return json.dumps(response)
 
